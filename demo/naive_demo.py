e3b2a00b25736f3e9a78be18b58ce82658143781
niuyazhe <niuyazhe@sensetime.com>
2022-05-05 18:58:29 +0800

    demo(nyz): add naive and ding demo

---
+++ demo/naive_demo.py
@@ -0,0 +1,38 @@
+import random
+import sys
+import time
+
+from ditk.hpo import hpo, R, M, choice, uniform, randint
+
+
+@hpo
+def opt_func(v):  # this function is still usable after decorating
+    x, y = v['x'], v['y']
+    time.sleep(0.1)
+    print("This [u]time's[/] config:", v)  # stdout will be captured
+    print("This is print line in stderr", file=sys.stderr)  # stderr will be captured
+    if random.random() < 0.5:  # randomly raise exception
+        raise ValueError('Fxxk this shxt')  # retry is supported
+
+    return {
+        'result': x * y,
+        'sum': x + y,
+    }
+
+
+if __name__ == '__main__':
+    print(opt_func.random()  # random algorithm
+          .max_steps(30)  # max steps
+          .minimize(R['result'])  # the maximize/minimize target you need to optimize,
+          .concern(M['time'], 'time_cost')  # extra concerned values (from metrics)
+          .concern(R['sum'], 'sum')  # extra concerned values (from return value of function)
+          .stop_when(R['result'] <= -800)  # conditional stop is supported
+          .spaces(  # search spaces
+        {
+            'x': uniform(-10, 110),  # continuous space
+            'y': randint(-10, 20),  # integer based space
+            'z': {
+                't': choice(['a', 'b', 'c', 'd', 'e']),  # enumerate space
+            },
+        }
+    ).run())
