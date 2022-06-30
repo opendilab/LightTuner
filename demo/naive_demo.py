0a8b8a69093ba40a1e9655d772eb2e8d7ba72ad9
HansBug <hansbug@buaa.edu.cn>
2022-06-30 13:19:19 +0800

    dev(hansbug): save naive model

--- demo/naive_demo.py
+++ demo/naive_demo.py
@@ -2,13 +2,13 @@ import random
 import time
 
 from ditk import logging
-from ditk.hpo import hpo, R, M, choice, uniform, randint
+from ditk.hpo import hpo, R, M, uniform, randint
 
 
 @hpo
 def opt_func(v):  # this function is still usable after decorating
     x, y = v['x'], v['y']
-    time.sleep(0.1)
+    time.sleep(5.0)
     logging.info(f"This [u]time's[/] config: {v}")  # stdout will be captured
     logging.warning("This is print line in stderr")  # stderr will be captured
     if random.random() < 0.5:  # randomly raise exception
@@ -22,18 +22,20 @@ def opt_func(v):  # this function is still usable after decorating
 
 if __name__ == '__main__':
     logging.try_init_root(logging.INFO)
-    print(opt_func.random()  # random algorithm
-          .max_steps(30)  # max steps
+    print(opt_func.bayes()  # bayes algorithm
+          .max_steps(100)  # max steps
+          .init_steps(20)  # init steps
+          .max_workers(10)  # max workers when paralleling
           .minimize(R['result'])  # the maximize/minimize target you need to optimize,
           .concern(M['time'], 'time_cost')  # extra concerned values (from metrics)
           .concern(R['sum'], 'sum')  # extra concerned values (from return value of function)
-          .stop_when(R['result'] <= -800)  # conditional stop is supported
+          # .stop_when(R['result'] <= -800)  # conditional stop is supported
           .spaces(  # search spaces
         {
             'x': uniform(-10, 110),  # continuous space
             'y': randint(-10, 20),  # integer based space
             'z': {
-                't': choice(['a', 'b', 'c', 'd', 'e']),  # enumerate space
+                't': uniform(-20, 10),  # enumerate space
             },
         }
     ).run())
