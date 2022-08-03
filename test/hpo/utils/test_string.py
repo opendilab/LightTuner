e8c2aba771b99ec70fc03d3742c03236859a3f60
HansBug <hansbug@buaa.edu.cn>
2022-08-03 14:56:23 +0800

    dev(hansbug): fix format problem

--- test/hpo/utils/test_string.py
+++ test/hpo/utils/test_string.py
@@ -5,5 +5,6 @@ from ditk.hpo.utils import rchain
 
 @pytest.mark.unittest
 class TestHpoUtilsString:
+
     def test_rchain(self):
         assert rchain([('name', 'str'), ('val', 233), ('float', 233.5)]) == "name: 'str', val: 233, float: 233.5"
